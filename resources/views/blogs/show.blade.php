e0ba378a4ef929dc4fb691b8810023c5fa4d5141
Basith Mohammed <basith374@gmail.com>
2015-06-04 23:27:54 +0530

    done

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -2,6 +2,35 @@
 
 @section('title', 'Blogs')
 
+@section('csslinks')
+<style type="text/css">
+.backb {
+	background: #111;
+	height: 250px;
+}
+#blog-banner {
+	background:url('/img/colorful_paint_splatter-wallpaper-1400x1050.jpg');
+	height: 250px;
+	text-align: center;
+	transition: all 0.2s linear;
+	vertical-align: middle;
+}
+#blog-banner > h1 {
+	transition: all 0.5s linear;
+	opacity: 0.1;
+	color: #333;
+	vertical-align: middle;
+}
+#blog-banner:hover {
+	opacity: 0.7;
+}
+#blog-banner:hover > h1 {
+	opacity: 0.9;
+	color: #ddd;
+}
+</style>
+@endsection
+
 @section('content')
 <div class="container">
 	<div class="row">
@@ -23,6 +52,58 @@
 				@endforeach
 			</div>
 		@endif
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="backb">
+					<div id="blog-banner"><h1>{{ $blog->title }}</h1></div>
+				</div>
+			</div>
+		</div>
+		<div class="top-link">
+			<a href="{{ route('blogs.index') }}" class="btn btn-default">Back</a>
+		</div>
+		<div class="col-lg-8 col-lg-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading"><h1>{{ $blog->title }}</h1></div>
+				<div class="panel-body">{{ $blog->body }}</div>
+				<div class="panel-footer">by {{ $blog->user->name }}<span class="badge pull-right">Posted {{ $blog->updated_at }}</span></div>
+			</div>
+		</div>
+		<div class="col-lg-12">
+			<h3>Comments</h3>
+			<div>
+			<form class="form-horizontal">
+				<div class="form-group">
+					<div class="col-lg-6">
+						<label class="label">Email</label>
+						<input type="text" class="form-control" />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-lg-6">
+						<label class="label">Comment</label>
+						<textarea class="form-control" rows="10"></textarea>
+					</div>
+				</div>
+				<input type="submit" class="btn btn-primary" value="Post Comment" />
+			</form>
+			</div>
+		</div>
 	</div>
 </div>
+@endsection
+
+@section('jslinks')
+<script type="text/javascript">
+// remember to move this code out to seperate js file. (Unobtrusive javascript)
+
+	$(document).scroll(function() {
+		var scrollPos = $(this).scrollTop();
+		$('#blog-banner').css({
+			//'top' : (scrollPos/3) + 'px',
+			'opacity' : 1-(scrollPos/280)
+		});
+	});
+
+</script>
 @endsection
